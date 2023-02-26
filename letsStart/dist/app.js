20197159d3a020fbeac1c0182cd23e3c605cd5c1
jinwapp <jinwapp@gmail.com>
2023-02-26 21:38:57 +0900

    ing

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -1,12 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
-var port = 8000;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var data = [1, 2, 3, 4];
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log('this is logging middleware');
+    next();
 });
-app.listen(port, function () {
-    console.log("Example app listening on port ".concat(port));
+app.get('/cats/som', function (req, res, next) {
+    console.log('this is som middleware');
+    next();
+});
+app.get('/', function (req, res) {
+    console.log('cats');
+    res.send({ cats: app_model_1.Cat });
+});
+app.get('/cats/blue', function (req, res) {
+    console.log('blue');
+    res.send({ cats: app_model_1.Cat, blue: app_model_1.Cat[0] });
+});
+app.get('/cats/som', function (req, res) {
+    console.log('som');
+    res.send({ som: app_model_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log('this is error middleware');
+    res.send({ error: '404 not found error' });
+});
+app.listen(8000, function () {
+    console.log('serve is on...');
 });
 //# sourceMappingURL=app.js.map
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,11 +1,43 @@
 import * as express from 'express';
-const app: express.Express = express();
-const port: number = 8000;
+import { Cat, CatType } from './app.model';
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+const app: express.Express = express();
+
+const data = [1, 2, 3, 4];
+
+app.use((req, res, next) => {
+	//next : 다음 라우터로 이동할 수 있는 함수
+	console.log(req.rawHeaders[1]);
+	console.log('this is logging middleware');
+	next();
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.get('/cats/som', (req, res, next) => {
+	//next : 다음 라우터로 이동할 수 있는 함수
+	console.log('this is som middleware');
+	next();
+});
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	console.log('cats');
+	res.send({ cats: Cat });
+});
+
+app.get('/cats/blue', (req: express.Request, res: express.Response) => {
+	console.log('blue');
+	res.send({ cats: Cat, blue: Cat[0] });
+});
+
+app.get('/cats/som', (req: express.Request, res: express.Response) => {
+	console.log('som');
+	res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+	console.log('this is error middleware');
+	res.send({ error: '404 not found error' });
+});
+
+app.listen(8000, () => {
+	console.log('serve is on...');
 });
